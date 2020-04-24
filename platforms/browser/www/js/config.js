66dc34db6b3d53f860067972b1b6da2915915010
karar78 <karar@live.com>
2020-04-24 15:44:08 +0100

    V1

--- platforms/browser/www/js/config.js
+++ platforms/browser/www/js/config.js
@@ -2,3 +2,12 @@
 host = "https://192.168.0.103:8443";
 //host = "https://192.168.1.5:8443/quiz";
 //host = "https://94.111.147.123:8443/quiz";
+
+function myFunction() {
+  var x = document.getElementById("navigation");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
